e257a53acad77fd40163e93b5822a064e0ce61c5
sband1 <sbandeali1@gmail.com>
2020-10-05 12:35:56 -0700

    Implement stringifyJSON fn code comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,26 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+// input: string, number, boolean, obj, array, func, null, undefined
+// output: '"string"', "9", "true", "{}", "[]"
+var stringifyJSON = function(input) {
+  // If this is string
+  // return double quoted string
+// if number
+  // return string number
+// if boolean
+  // return string version
+// if nulll
+  // return string null
+// if undefined
+  // return
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+  // if array
+  // create empty array var
+  // iterate through input
+  // empty arr.push (recursion(input))
+  // return Join the array with commas and surrond by brackets
+  // if obj
+  // return string obj
+
 };
